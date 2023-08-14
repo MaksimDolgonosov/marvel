0c16a35c4b3d1fcb2f93c5fd17a461ed314a018a
MAKSIM <max_air@bk.ru>
2023-08-14 22:02:34 +0300

    Second

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,7 @@
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
+
 import CharInfo from "../charInfo/CharInfo";
 
 import decoration from '../../resources/img/vision.png';
